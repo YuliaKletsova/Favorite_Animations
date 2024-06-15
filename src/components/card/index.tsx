899f662b0ade6a46f2c126c8b10c0778698de2bf
Yulia Kletsova <july_2000@mail.ru>
2024-06-15 05:22:38 +0400

    experiement: types for animations

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,14 @@
 'use client';
 import React, { useState } from 'react';
+import { ANIMATION_TYPE, Animations } from '~/constants';
 import { Modal } from '../modal';
 
 // Function to get a random span class
-const getSpan = () => {
+const getSpan = (type?: Animations) => {
+    if (type === ANIMATION_TYPE.TEXT) {
+        return 'md:col-span-1';
+    }
+
     const random = Math.floor(Math.random() * 4) + 1;
     return `md:col-span-${random}`;
 };
@@ -11,7 +16,11 @@ const getSpan = () => {
 export const CssGrid = ({
     data,
 }: {
-    data: { title: string; modalContent: JSX.Element }[];
+    data: {
+        title: string;
+        modalContent: JSX.Element;
+        type?: Animations;
+    }[];
 }) => {
     const [activeItem, setActiveItem] = useState<number | null>(null);
     const boxStyle =
@@ -20,10 +29,10 @@ export const CssGrid = ({
     return (
         <>
             <div className="w-full grid grid-flow-row-dense md:grid-cols-4 auto-rows-[100px] gap-4 my-10">
-                {data.map(({ title }, i) => (
+                {data.map(({ title, type }, i) => (
                     <div
                         key={i}
-                        className={`${boxStyle} ${getSpan()}`}
+                        className={`${boxStyle} ${getSpan(type)}`}
                         onClick={() => setActiveItem(i)}
                     >
                         <div className="text-xl text-midnight font-semibold">
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export enum ANIMATION_TYPE {
+    TEXT = 'TEXT',
+}
+
+export type Animations = keyof typeof ANIMATION_TYPE;
